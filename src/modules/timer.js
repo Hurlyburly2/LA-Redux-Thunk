e3c078ea0ee85d865bdc1f0a76ee063569e00dd9
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-13 00:55:43 -0400

    filled out timer reducer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,12 +1,43 @@
-const initialState = {}
+const initialState = {
+  isRunning: false,
+  sendAlert: false
+}
+
+const START_TIMER = 'START_TIMER'
+const startTimer = () => {
+  return {
+    type: START_TIMER
+  }
+}
+
+const END_TIMER_SUCCESSFULLY = 'END_TIMER_SUCCESSFULLY'
+const endTimerSuccessfully = () => {
+  return {
+    type: END_TIMER_SUCCESSFULLY
+  }
+}
 
 const timer = (state = initialState, action) => {
   switch(action.type) {
+    case START_TIMER:
+      return {
+        ...state,
+        isRunning: true,
+        sendAlert: false
+      }
+    case END_TIMER_SUCCESSFULLY:
+      return {
+        ...state,
+        isRunning: false,
+        sendAlert: true
+      }
     default:
       return state
   }
 }
 
 export {
-  timer
+  timer,
+  startTimer,
+  endTimerSuccessfully
 }
